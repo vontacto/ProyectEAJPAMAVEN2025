70c052208487d2cc1302c84493d812f5b46f3601
vontacto <rios8mario@gmail.com>
2025-02-01 20:34:23 -0500

    version 4.0

---
+++ src/main/java/Controlador/ControladorLogueo.java
@@ -0,0 +1,50 @@
+package Controlador;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ControladorLogueo
+ */
+public class ControladorLogueo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ControladorLogueo() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		
+		
+		//fin del metodo doget...
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		
+		//redireccionamos
+		request.getRequestDispatcher("MenuPrincipal.jsp").forward(request, response);;
+		
+		//fin del metodo doPost...
+				
+		
+	}
+
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,29 @@
 <html>
-<body>
-<h2>Hello World!</h2>
+<body bgcolor=#o5dec9>
+<h2 align="center">Logeo al sistema web de Almacen</h2>
+
+<h2 align="center">Version 1.0</h2>
+<h2 align="center"> Logeo </h2>
+<table border="2" border="2" align="center">
+<form method="post" action="ControladorLogueo">
+<tr>
+<td>Usuario</td>
+<td><input type="text" name="usuario"></td>
+</tr>
+
+<tr>
+<td>Password</td>
+<td><input type="password" name="password"></td>
+</tr>
+
+
+<tr>
+<td colspan="2" align="center"><input type="submit" value="ingresar al sistema"></td>
+</tr>
+
+</form>
+
+</table>
+
 </body>
 </html>
